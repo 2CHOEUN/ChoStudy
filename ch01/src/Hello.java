5607987a29bc54b7b44fdb6647bccdf2c4b44dcd
DESKTOP-OJAKMJD\wldns <qazqaz1218@naver.com>
2023-07-14 22:27:47 +0900

    자바의 정석 ch01 공부내용

---
+++ ch01/src/Hello.java
@@ -0,0 +1,30 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+        /* 여러줄 주석
+        
+        
+        ctrl + shift + L : 단축키 전체 목록보기
+        ctrl + +,- : 폰트 크기 조절
+        ctrl + D : 한 줄 삭제
+        ctrl + alt + down : 행단위 복사
+        window > preference > General > Keys : 단축키 변경
+        alt + shift + A : 멀티컬럼 편집
+        alt + up,down : 행단위 이동
+        tab :  들여쓰기 / Shift + tab : 내어쓰기
+        ctrl + I : 자동 들여쓰기
+        ctrl + / : 주석
+        ctrl + space : 자동완성
+        
+        window > preference > General > Java > Editor > Templates
+        : 자동완성 내용 변경
+        
+        window > preference > General > Java > Editor > Content Assist
+        > Auto Activation  : 자동완성 설정
+         */
+        
+        System.out.println("Hello, world");
+	}
+
+}
