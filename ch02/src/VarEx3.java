8f60cb29c177ba18492f4711f02adb727bf0c830
DESKTOP-OJAKMJD\wldns <qazqaz1218@naver.com>
2023-07-18 22:56:19 +0900

    ch02-

--- ch02/src/VarEx3.java
+++ ch02/src/VarEx3.java
@@ -21,11 +21,24 @@ public class VarEx3 {
 		
 		float f = 3.14f;// f를 제거하면 에러
 		double d = 3.14;
-		System.out.println(10.);
-		System.out.println(.10);
-		System.out.println(10f);
-		System.out.println(1e3);
+		System.out.println("10.=" + 10.);
+		System.out.println(".10=" +.10);
+		System.out.println("10f=" + 10f);
+		System.out.println("le3=" + 1e3);
 		
+		char ch = 'A';//하나의 문자만 들어가야한다.
+		int i = 'A';
+		System.out.println("ch=" + ch);
+		System.out.println("i=" + i);
+		
+		String str = "";
+		String str2 = "ABCD";
+		String str3 = "123";
+		String str4 = "str2+str3 = " + str2+str3;
+		
+		System.out.println(str4);
+		System.out.println("" + 7 + 7);
+		System.out.println(7 + 7 + "");
 	}
 
 }
